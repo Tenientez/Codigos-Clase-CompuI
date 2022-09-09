b559ceae3be6e088972f7daf26e8bbb175a0f49c
Oscar Teniente <111546056+Tenientez@users.noreply.github.com>
2022-09-09 12:06:09 -0500

    Add files via upload

---
+++ Arreglo.cpp
@@ -0,0 +1,24 @@
+// Autor: Oscar Teniente
+// tenienteornelas@gmail.com
+// 09 Septiembre 2022
+
+#include <iostream>
+using namespace std;
+
+// Define: Para definir una varibale por un múmero
+#define N 10
+
+int main() {
+
+    // Definición de un arreglo
+    int arr[N] { 1 };
+
+    // Iterar arreglo
+    for (int i=0 ; i < N ; i++) {
+
+        //Al arreglo en la posición de i se agrega un valor
+        arr[i] = i + i ;
+        cout << arr[i] << "\t";
+    }
+    return 0;
+}
