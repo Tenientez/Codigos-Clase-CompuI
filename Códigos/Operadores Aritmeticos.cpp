072775ae9f564a42adcd709891935185d2ecb71c
Oscar Teniente <111546056+Tenientez@users.noreply.github.com>
2022-09-18 19:49:43 -0500

    Operadores Aritméticos

---
+++ Códigos/Operadores Aritmeticos.cpp
@@ -0,0 +1,20 @@
+// Autor: Oscar Teniente
+// tenienteornelas@gmail.com
+
+#include <iostream>
+
+using namespace std;
+
+//Se declaran 2 variables
+int main() {
+    int a=23,b=12;
+
+//Utilizar Operadores + - / * %
+    cout << "La suma de " << a << " + " << b << " es " << a+b << endl;
+    cout << "La resta de " << a << " - " << b << " es " << a-b << endl;
+    cout << "La división de " << a << " / " << b << " es " << a/b << endl;
+    cout << "La multiplicación de " << a << " * " << b << " es " << a*b << endl;
+    cout << "El módulo de " << a << " % " << b << " es " << a%b << endl;
+
+    return 0;
+}
