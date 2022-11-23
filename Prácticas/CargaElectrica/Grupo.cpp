d08b0b12d4df03f16e6d3e9762b6016cffbba7da
Oscar Teniente <111546056+Tenientez@users.noreply.github.com>
2022-11-23 10:58:03 -0600

    Update Grupo.cpp

--- Prácticas/CargaElectrica/Grupo.cpp
+++ Prácticas/CargaElectrica/Grupo.cpp
@@ -1,22 +1,4 @@
-//
-// Created by V303_11 on 14/11/2022.
-//
-#include "Grupo.h"
-#include <string>
-#include <array>
 
-using namespace std;
-
-string Grupo::getNombre()
-{
-    return nombreGrupo;
-}
-float Grupo::getpk() {
-    return pkGrupo;
-}
-int Grupo::getCarga() {
-    return cargaGrupo;
-}
 
 #include "Grupo.h"
 #include <string>
@@ -25,13 +7,15 @@ int Grupo::getCarga() {
 using namespace std;
 
 std::string Grupo::getNombre() {
-    return NombreGrupo;
+    return nombreGrupo;
 
 }
-float Grupo::getPk() {
+float Grupo::getpK() {
     return pKGrupo;
 }
 
-int Grupo::getCargagrupo() {
+int Grupo::getCarga() {
+    return cargaGrupo;
+}
     return CargaGrupo;
 }
